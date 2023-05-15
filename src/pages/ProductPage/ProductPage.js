13e09ccb0e59eb3501531a983beb2af1a09bc99f
Zac <121649297+zac-march@users.noreply.github.com>
2023-05-16 08:13:30 +1000

    Add Product page

--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import style from "./ProductPage.module.css";
+import { Link } from "react-router-dom";
 
 const ProductPage = (props) => {
+  const [product, setProduct] = useState();
+  const { productId } = useParams();
   const { data } = props;
-  return <div></div>;
+
+  useEffect(() => {
+    setProduct(data.filter((entry) => entry.id === productId)[0]);
+  }, []);
+
+  return (
+    <div className={style.page}>
+      <div class={style.header}>
+        <Link to="/store">
+          <button className={style.returnBtn}>← Store</button>
+        </Link>
+        <h1>{product && product.title}</h1>
+      </div>
+      {product && (
+        <div className={style.product}>
+          <div className={style.images}>
+            <img src={product.cover_image}></img>
+          </div>
+          <div className={style.info}>
+            <h3>About</h3>
+            <p>
+              Mauris placerat dolor in neque mattis luctus. Pellentesque
+              dignissim dui nisi, vel laoreet massa semper vitae. Maecenas id
+              dignissim metus. Pellentesque habitant morbi tristique senectus et
+              netus et malesuada fames ac turpis egestas. Quisque aliquam purus
+              magna, varius faucibus sem placerat maximus. Nam dictum sit amet
+              quam vel vulputate. Cras imperdiet, risus non tempor consectetur,
+              arcu velit cursus nulla, at condimentum metus diam ac nisi. Morbi
+              massa neque, ornare sed eros vel, posuere consectetur velit.
+            </p>
+
+            <div>
+              <p>$70</p>
+              <button>Add to Cart</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ProductPage;
--- src/pages/StorePage/StorePage.js
+++ src/pages/StorePage/StorePage.js
@@ -1,40 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import style from "./StorePage.module.css";
-import productData from "../../data/data.json";
 import uniqid from "uniqid";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import { Link } from "react-router-dom";
 
-const StorePage = () => {
-  const [products, setGames] = useState([]);
-
-  useEffect(() => {
-    getFilteredData();
-  }, []);
-
-  const getFilteredData = () => {
-    console.log(productData);
-    const filteredData = productData.results.map(
-      ({ name, background_image, parent_platforms }) => ({
-        title: name,
-        cover_image: background_image,
-        platforms: parent_platforms,
-      })
-    );
-
-    filteredData.forEach((data) => {
-      data.platforms = data.platforms.map((item) => item.platform.name);
-      data["id"] = "product_" + uniqid();
-    });
-
-    console.log(filteredData);
-
-    setGames(filteredData);
-  };
+const StorePage = (props) => {
+  const { data } = props;
 
   function handleProduct() {}
 
   return (
-    <div className={style.browse}>
+    <div className={style.store}>
       <div className={style.header}>
         <h1>Featured and recommended</h1>
         <p>
@@ -42,9 +18,15 @@ const StorePage = () => {
           latest releases to timeless classics.
         </p>
       </div>
-      <div className={style.games}>
-        {products.map((game) => (
-          <ProductCard key={uniqid()} data={game} onClick={handleProduct} />
+      <div className={style.products}>
+        {data.map((product) => (
+          <Link
+            key={uniqid()}
+            to={`/store/${product.id}`}
+            className="routeLink"
+          >
+            <ProductCard data={product} onClick={handleProduct} />
+          </Link>
         ))}
       </div>
     </div>
