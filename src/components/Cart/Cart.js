f0581800211f34a8c960fd09c9872428442e21f2
Zac <121649297+zac-march@users.noreply.github.com>
2023-05-23 20:53:53 +1000

    Add Cart total

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,29 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Cart.module.css";
 
 const Cart = (props) => {
   const { isOpen, toggleCart, cart, removeFromCart } = props;
+  const [total, setTotal] = useState();
+
+  function getTotal() {
+    setTotal(cart.reduce((acc, item) => acc + item.price, 0));
+  }
+
+  useEffect(() => {
+    getTotal();
+  }, [cart]);
+
   return (
     isOpen && (
       <div className={style.container}>
-        <div onClick={toggleCart} className={style.background}></div>
         <div className={style.cart}>
-          <h1>
-            {cart.length === 0 ? "No" : cart.length} Game
-            {(cart.length === 0 || cart.length > 1) && "s"} Added
-          </h1>
-          <ul>
-            {cart.map((product) => (
-              <div className={style.cartItem}>
-                <p>{product.title}</p>
-                <div>
-                  <p>$20</p>
-                  <button onClick={() => removeFromCart(product.id)}>X</button>
+          <div className={style.cartTop}>
+            <h1>
+              {cart.length === 0 ? "No" : cart.length} Game
+              {(cart.length === 0 || cart.length > 1) && "s"} Added
+            </h1>
+            <ul>
+              {cart.map((product) => (
+                <div key={product.id} className={style.cartItem}>
+                  <p>{product.title}</p>
+                  <div>
+                    <p>${product.price / 100}</p>
+                    <button
+                      className="btn"
+                      onClick={() => removeFromCart(product.id)}
+                    >
+                      X
+                    </button>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </ul>
+              ))}
+            </ul>
+          </div>
+          <div className={style.cartBottom}>
+            <p>Total: ${total / 100}</p>
+            <button className="btn">Checkout -{">"}</button>
+          </div>
         </div>
+        <div onClick={toggleCart} className={style.background}></div>
       </div>
     )
   );
